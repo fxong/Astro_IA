55e7e4a08f98314867c4216f93bedf6dcf2f4fe3
fxong <43685541+fxong@users.noreply.github.com>
2018-11-09 16:09:47 -0500

    Add files via upload

---
+++ OpDep.py
@@ -0,0 +1,67 @@
+""" 
+Purpose:
+    calculate the optical depths of two isotopologues
+Usage:
+Caution:
+    the input fitsfiles must be 2D and should have already been applied by 3*rms cutoff
+    the tracer in fitsfile1 is assumed to be optically thicker than the tracer in fitsfile2
+    the optical depth of tracer in fitsfile2 should be in the range 0.00~2.00
+History:
+    Completed. Version 0.8.0. Nov. 07, 18.
+Copyright:
+    written by fxong@CfA
+"""
+
+from astropy.io import fits as ft
+import numpy as np
+
+fitsfile1='mosaic_U_m0_3rms' # without suffix
+fitsfile2='mosaic_L_m0_3rms' # without suffix
+ratio=60 # optical depth ratio, [tracer in fitsfile1]/[tracer in fitsfile2], for molecular gas near solar system, the ratio is about 50~60
+step=0.001
+
+dat1, hdr1=ft.getdata(fitsfile1+'.fits', header=True)
+dat2, hdr2=ft.getdata(fitsfile2+'.fits', header=True)
+
+for i in range(dat1.shape[0]):
+    for j in range(dat1.shape[1]):
+        if dat1[i,j]==0: dat1[i,j]=np.nan
+for i in range(dat2.shape[0]):
+    for j in range(dat2.shape[1]):
+        if dat2[i,j]==0: dat2[i,j]=np.nan
+dat1_dat2=dat1/dat2
+minimum=np.nanmin(dat1_dat2)
+maximum=np.nanmax(dat1_dat2)
+mean=np.nanmean(dat1_dat2)
+median=np.nanmedian(dat1_dat2)
+
+tau2=np.arange(step, 2+step, step)
+tau1=tau2*ratio
+T1_T2=(1-np.exp(-tau1))/(1-np.exp(-tau2))
+
+# tau2_dat=np.zeros_like(dat1_dat2)
+# tau1_dat=np.zeros_like(dat1_dat2)
+# for i in range((dat1_dat2).shape[0]):
+#     for j in range((dat1_dat2).shape[1]):
+#         if np.isnan(dat1_dat2[i,j]):
+#             tau2_dat[i,j]=np.nan
+#             tau1_dat[i,j]=np.nan
+#         if np.invert(np.isnan(dat1_dat2[i,j])):
+#             index=np.argmin(abs(T1_T2-dat1_dat2[i,j]))
+#             tau2_dat[i,j]=tau2[index]
+#             tau1_dat[i,j]=tau1[index]
+index1=np.argmin(abs(T1_T2-minimum))
+index2=np.argmin(abs(T1_T2-maximum))
+index3=np.argmin(abs(T1_T2-mean))
+index4=np.argmin(abs(T1_T2-median))
+
+print('brigthness temperature ratio (tracer in %s.fits / tracer in %s.fits):' %(fitsfile1, fitsfile2))
+print('(1)min: %f; (2)max: %f; (3)mean: %f; (4)median: %f' %(minimum, maximum, mean, median))
+print('When applying the min/max/mean/median brigthness temperature ratio, the corresponding optical depth of tracer in %s.fits:' %(fitsfile1))
+print('(1): %f; (2): %f; (3): %f; (4): %f' %(tau1[index1], tau1[index2], tau1[index3], tau1[index4]))
+print('When applying the min/max/mean/median brigthness temperature ratio, the corresponding optical depth of tracer in %s.fits:' %(fitsfile2))
+print('(1): %f; (2): %f; (3): %f; (4): %f' %(tau2[index1], tau2[index2], tau2[index3], tau2[index4]))
+# print('optical depth of tracer in %s.fits:' %(fitsfile1))
+# print('min: %f; max: %f; mean: %f; median: %f' %(np.nanmin(tau1_dat), np.nanmax(tau1_dat), np.nanmean(tau1_dat), np.nanmedian(tau1_dat)))
+# print('optical depth of tracer in %s.fits:' %(fitsfile2))
+# print('min: %f; max: %f; mean: %f; median: %f' %(np.nanmin(tau2_dat), np.nanmax(tau2_dat), np.nanmean(tau2_dat), np.nanmedian(tau2_dat)))
